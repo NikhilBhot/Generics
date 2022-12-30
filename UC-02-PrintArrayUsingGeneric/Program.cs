92f6615f6bc788e2aab84e585193d55d5066e9f4
unknown <bhotnikhil25@gmail.com>
2022-12-30 12:25:56 +0530

    [Nikhil]-Print array Using Generics

---
+++ UC-02-PrintArrayUsingGeneric/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace UC_02_PrintArrayUsingGeneric
+{
+    public class Program
+    {
+
+        public static void toPrint<T>(T[] array)
+        {
+            foreach(var element in array)
+            {
+                System.Console.WriteLine(element);
+            }
+            Console.WriteLine("_________________________________");
+        }
+        static void Main(string[] args)
+        {
+
+            int[] intArray = { 1, 2, 3 };
+            double[] doubleArray= { 1.1, 2.1, 3.1 };
+            char[] charArray = { 'a', 'b', 'c' };
+
+            Program.toPrint<int>(intArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<char>(charArray);
+        }
+    }
+}
