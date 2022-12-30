68467d46aee19d9e3b628d06b2ea50b23ad2801a
unknown <bhotnikhil25@gmail.com>
2022-12-30 12:33:48 +0530

    [Nikhil]-Refactor-1

---
+++ Refactor-1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Refactor_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Problem using Generics");
+
+            double n = RefactorCode1<double>.MaxValue(50.5, 45.6, 48.5);
+            int a = RefactorCode1<int>.MaxValue(70, 60, 50);
+            string c = RefactorCode1<string>.MaxValue("Peach", "Apple", "Banana");
+
+
+
+            Console.WriteLine("Maximum int in is: {0}", a);
+            Console.WriteLine("Maximum Number in float is: {0}", n);
+            Console.WriteLine("Maximum value in string is: {0}", c);
+        }
+    }
+}
