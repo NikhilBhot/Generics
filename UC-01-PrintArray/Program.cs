f166c6b32d5d41c7104fcff77c2a5d6a0493d2c7
unknown <bhotnikhil25@gmail.com>
2022-12-30 12:24:18 +0530

    [Nikhil]-Print Simple Array

---
+++ UC-01-PrintArray/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Given an array of Integer,
+Double and Character, write
+a program to print the same - Create PrintArray class and define
+toPrint method to print
+corresponding elements to console
+ */
+
+namespace UC_01_PrintArray
+{
+    class Program
+    {
+       public static void toPrint(int[] inputArray)
+        {
+            foreach(int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            //Console.WriteLine("_____________________________________");
+        }
+        public static void toPrint(double[] inuputArray)
+        {
+            foreach(double element in inuputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("___________________________");
+
+        }
+        public static void toPrint(char[] inuputArray)
+        {
+            foreach (char element in inuputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("___________________________");
+
+        }
+
+        public static void Main(String[]args)
+        {
+            int[] intArray = { 1, 2, 3};
+            double[] doubleArray = { 1.1, 1.2, 1.3 };
+            char[] charArray = { 'a', 'b', 'c', 'd', };
+
+            Program.toPrint(intArray);
+            Program.toPrint(doubleArray);
+            Program.toPrint(charArray);
+
+        }
+    }
+}
